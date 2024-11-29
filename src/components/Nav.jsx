b885d39633ed319fc93f1fcaa4b8d7afae615d4f
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-29 19:30:39 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,68 @@
-import { hamburger } from "../assets/icons";
+"use client";
+
 import { headerLogo } from "../assets/images";
+import { hamburger, close, cart } from "../assets/icons";
 import { navLinks } from "../constants";
+import React, { useState } from "react";
+import { useColorMode, useColorModeValue } from "@chakra-ui/react";
+
+import { IoMoon, IoSunny } from "react-icons/io5";
 
 const Nav = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("gray.600", "gray.300");
+
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  React.useEffect(() => {
+    console.log("Color mode changed:", colorMode);
+  }, [colorMode]);
+
+  const handleToggle = () => {
+    console.log("Toggle button clicked");
+    toggleColorMode();
+  };
+
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <a href='/'>
-          <img
-            src={headerLogo}
-            alt='logo'
-            width={129}
-            height={29}
-            className='m-0 w-[129px] h-[29px]'
-          />
+    <header className={`padding-x py-8 absolute z-10 w-full bg-${bg}`}>
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-          {navLinks.map((item) => (
-            <li key={item.label}>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.id}>
               <a
-                href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                href={link.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray hover:font-bold"
               >
-                {item.label}
+                {link.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
-          <a href='/'>Sign in</a>
-          <span>/</span>
-          <a href='/'>Explore now</a>
+
+        <div className="justify-center items-center px-6">
+          <div
+            className="justify-center items-center w-[10px] h-[30px] cursor-pointer rounded-xl sm:px-1 md:px-2"
+            onClick={handleToggle}
+          >
+            {colorMode === "light" ? (
+              <IoMoon className="text-2xl" />
+            ) : (
+              <IoSunny className="text-2xl" />
+            )}
+          </div>
         </div>
-                <div className="hidden max-lg:block">
+
+        <div className="flex px-6 gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
+
+        <div className="hidden max-lg:block">
           <img
             src={toggle ? close : hamburger}
             alt="Hamburger"
